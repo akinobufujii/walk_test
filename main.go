c5bed41e7b9c68790048c9d45afda63ec01f847b
akinobufujii <aki.develop@gmail.com>
2021-10-17 17:05:16 +0900

    かんたんな検証コード追加

--- main.go
+++ main.go
@@ -48,19 +48,36 @@ func findFilesWithWalkDir(root string) ([]string, error) {
 func main() {
 	SearchPath := os.ExpandEnv("${GOPATH}")
 
-	findFiles, err := findFilesWithWalk(SearchPath)
+	findFilesWalk, err := findFilesWithWalk(SearchPath)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	fmt.Printf("findFilesWithWalk %v\n", len(findFiles))
-
-	findFiles, err = findFilesWithWalkDir(SearchPath)
+	findFilesWalkDir, err := findFilesWithWalkDir(SearchPath)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	fmt.Printf("findFilesWithWalkDir %v\n", len(findFiles))
+	// 結果一致確認
+	// まずは検出数が同じか見る
+	fmt.Printf("findFilesWithWalk %v\n", len(findFilesWalk))
+	fmt.Printf("findFilesWithWalkDir %v\n", len(findFilesWalkDir))
+
+	// 次に内容一致確認
+	// 検出数が同じ状態で片方をmapのキーにして、その後キーをもう片方の要素で削除したとき
+	// 0になれば一致したと言えるはず
+	findMap := map[string]struct{}{}
+	for _, filename := range findFilesWalk {
+		findMap[filename] = struct{}{}
+	}
+
+	fmt.Printf("findMap %v\n", len(findMap))
+
+	for _, filename := range findFilesWalkDir {
+		delete(findMap, filename)
+	}
+
+	fmt.Printf("findMap %v\n", len(findMap))
 }
